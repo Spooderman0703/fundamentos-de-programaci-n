cb690cec55eb17b58c7010cc6268c5d6b7cb3d09
Alejandro Mariño <alejandromm03@homtail.com>
2026-09-11 15:57:13 -0600

    Submenú Diccionarios

---
+++ Semana 5/actividad5_menu_modular.py
@@ -0,0 +1,83 @@
+def suma_lista(valores):
+    total = 0
+    for i in valores:
+        total += i
+    return total
+def gestionar_tuplas(numeros):
+    print("\n--- SUBMENÚ TUPLAS ---")
+    print("1. Ver elementos.")
+    print("2. Ver tercer elemento.")
+    print("3. Agregar elementos, ordenar, mostrar y sumar valores de nueva tupla.")
+    print("4. Sumar elementos de tupla original.")
+    
+    sub_opcion = int(input("Selecciona una opción: "))
+    
+    if sub_opcion == 1:
+        print(f"Los elementos son: {numeros}")
+    elif sub_opcion == 2:
+        print(f"El tercer elemento es: {numeros[2]}")
+    elif sub_opcion == 3:
+        num1 = int(input("Ingresa el primer número: "))
+        num2 = int(input("Ingresa el segundo número: "))
+        nueva_tupla = numeros + (num1, num2)
+        lista_ordenada = sorted(list(nueva_tupla), reverse=True)
+        print(f"Nueva lista ordenada: {lista_ordenada}")
+        print(f"Suma de la nueva tupla: {suma_lista(lista_ordenada)}")
+    elif sub_opcion == 4:
+        print(f"La suma total de la tupla orignial es: {suma_lista(numeros)}")
+def buscar_telefono(contactos, nombre):
+    return contactos.get(nombre, None)
+def gestionar_diccionarios(contactos):
+    print("\n--- SUBMENÚ DICCIONARIOS ---")
+    print("1. Mostrar elementos de diccionario/directorio.")
+    print("2. Añadir nuevo contacto (nombre y número telefónico).")
+    print("3. Mostrar todos los nombres del directorio.")    
+    print("4. Buscar nombre y mostrar número telefónico.")
+    
+    sub_opcion = int(input("Selecciona una opción: "))
+    
+    if sub_opcion == 1:
+        print(f"Directorio actual: {contactos}")
+    elif sub_opcion == 2:
+        nuevo_nombre = input("Ingresa el nombre del contacto: ")
+        nuevo_num_tel = int(input("Ingresa su número telefónico: "))
+        contactos[nuevo_nombre] = nuevo_num_tel
+        print(f"Se agregó al directorio el contacto {nuevo_nombre}, con su número {nuevo_num_tel}")
+    elif sub_opcion == 3:
+        print("Los contactos registrados del directorio base son: ")
+        for n in contactos.keys():
+            print(n, end=", ")
+    elif sub_opcion == 4:
+        buscar_nombre = input("Ingresa el nombre del contacto que buscas: ")
+        telefono = buscar_telefono(contactos, buscar_nombre)
+
+        if telefono:
+            print(f"Número de {buscar_nombre} encontrado. Su número es: {telefono}")
+        else:
+            print("Contacto y número no encontrados.")
+        
+
+
+
+def menu_principal():
+    numeros = (1234, 4567, 7890, 1357, 2468)
+    contactos = {
+        "Dano" : 4461319445,
+        "Iris" : 4428100553,
+        "Ulises" : 4427510326
+    }
+    
+    while True:
+        print("\n--- MENÚ PRINCIPAL ---")
+        print("1. Tuplas\n2. Diccionarios\n3. Excepciones\n4. Strings\n5. Finalizar")
+        opcion = int(input("Selecciona una opción: "))
+        
+        if opcion == 1:
+            gestionar_tuplas(numeros)
+        if opcion == 2:
+            gestionar_diccionarios(contactos)
+        elif opcion == 5:
+            print("Programa finalizado.")
+            break
+
+menu_principal()
